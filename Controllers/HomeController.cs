7841f87b684225036175c050a027ebf34334e609
Matyáš Novotný <novotny.matyas@ssakhk.cz>
2023-10-19 13:28:58 +0200

    detail, landing edit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -42,6 +42,11 @@ namespace NajdiDoktoraApp.Controllers
             return View();
         }
 
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
